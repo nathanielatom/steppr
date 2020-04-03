07e8c5d00b8f8286eb45a7c5414c49f6269a7698
Adam Suban-Loewen <adam.subanloewen@gmail.com>
2020-04-03 01:15:06 -0400

    added rests and CLI

--- play_midi.py
+++ play_midi.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 import gpiozero
 from mido import MidiFile
 import librosa
@@ -6,9 +6,16 @@ import librosa
 stepper_pin = "GPIO12" # supports hardware PWM
 direction_pin = "GPIO25"
 
+parser = argparse.ArgumentParser(description='Play MIDI files on a stepper motor.')
+parser.add_argument('filename', type=str, help="The MIDI file.")
+parser.add_argument('--channel', '-ch', type=int, default=0, help="Which MIDI channel to play (in all tracks).")
+parser.add_argument('--rests', '-r', action='store_true', default=False, help="Whether to play rests or not.")
+
 if __name__ == '__main__':
-    filename = sys.argv[-1]
-    channel = 0
+    args = parser.parse_args()
+    filename = args.filename
+    channel = args.channel
+    rests = args.rests
     duty_cycle = 0.2
 
     midi = MidiFile(filename)
@@ -16,13 +23,15 @@ if __name__ == '__main__':
                                             initial_value=0)
     stepper = gpiozero.PWMOutputDevice(pin=stepper_pin,
                                        initial_value=0,
-                                       frequency=0)
+                                       frequency=100)
 
-    stepper.value = duty_cycle
     for msg in midi.play():
         if msg.type == 'note_on' and msg.channel == channel:
+            stepper.value = duty_cycle
             frequency = librosa.midi_to_hz(msg.note)
             stepper.frequency = frequency
             # duration = msg.time # s
             # amplitude = msg.velocity
+        if rests and msg.type == 'note_off' and msg.channel == channel:
+            stepper.value = 0
     stepper.value = 1
